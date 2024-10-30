bd9fabd38b1a61fdcf29ad5c0d1e1541714c65e5
GRandles16 <gtarandles@gmail.com>
2024-10-30 22:58:44 +0000

    Create Escaping_the_Maze.py
    First visit reeborg.ca and go the maze challenge

---
+++ Day_006_Escaping_the_Maze/Escaping_the_Maze.py
@@ -0,0 +1,16 @@
+# Enter code into the Maze challenge on reeborg.class
+# link here: https://shorturl.at/tGWf8
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
